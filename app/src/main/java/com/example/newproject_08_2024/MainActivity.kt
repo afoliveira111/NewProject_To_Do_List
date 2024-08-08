fa6f46fcdd3707d882a28488e4cf655300accf3d
Felipe <felipetermometria@hotmail.com>
2024-08-08 14:21:07 -0300

    Main updates

--- app/src/main/java/com/example/newproject_08_2024/MainActivity.kt
+++ app/src/main/java/com/example/newproject_08_2024/MainActivity.kt
@@ -2,15 +2,34 @@ package com.example.newproject_08_2024
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val taskViewModel: TaskViewModel by viewModels()
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: TaskAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recyclerview_id)
+        adapter = TaskAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        taskViewModel.allTasks.observe(this, Observer { tasks ->
+            tasks?.let { adapter.submitList(it) }
+        })
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
